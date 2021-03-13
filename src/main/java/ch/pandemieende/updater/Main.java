90bd974cee60c8d66bd1effc8b0fff077dea153f
Marcus Fihlon <marcus@fihlon.swiss>
2021-03-13 17:27:21 +0100

    :heavy_plus_sign: Adding Apache Commons CLI library

--- src/main/java/ch/pandemieende/updater/Main.java
+++ src/main/java/ch/pandemieende/updater/Main.java
@@ -28,18 +28,38 @@ package ch.pandemieende.updater;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public final class Main {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
-    private static final String APPNAME = "Pandemieende Updater";
     private static final String VERSION = loadVersionString();
     private static final String LICENSE = "MIT License - Copyright (C) 2021 Marcus Fihlon and the individual contributors to Pandemieende Updater.";
 
     public static void main(@NonNull final String... args) {
-        System.out.printf("%s %s%n%s%n", APPNAME, VERSION, LICENSE);
+        final var options = buildOptions();
+        try {
+            final var cmd = parseCommandLine(options, args);
+            if (cmd.hasOption("u")) {
+                System.out.println("Do something useful...");
+            }
+            if (cmd.hasOption("h") || cmd.getOptions().length <= options.getRequiredOptions().size()) {
+                printHelp(options);
+            }
+            if (cmd.hasOption("v")) {
+                System.out.printf("CSS Slack Bot %s%n%s%n", VERSION, LICENSE); //NOSONAR
+            }
+        } catch (final ParseException e) {
+            LOGGER.error(e.getMessage());
+            printHelp(options);
+        }
     }
 
     private static String loadVersionString() {
@@ -51,6 +71,28 @@ public final class Main {
             LOGGER.error(e.getMessage(), e);
             return "<UNKNOWN VERSION>";
         }
+    }
+
+    private static CommandLine parseCommandLine(final @NonNull Options options, final String... args) throws ParseException {
+        return new DefaultParser().parse(options, args);
+    }
+
+    private static Options buildOptions() {
+        final var options = new Options();
+        options.addOption("h", "help", false, "print this message");
+        options.addOption("u", "update", false, "update the pandemic data");
+        options.addOption("v", "version", false, "print version information");
+        return options;
+    }
+
+    private static void printHelp(@NonNull final Options options) {
+        final var formatter = new HelpFormatter();
+        formatter.printHelp(
+                "updater",
+                "Automating the update process for the \"pandemieende.ch\" website.\n ",
+                options,
+                "\n" + LICENSE,
+                true);
     }
 
     private Main() throws InstantiationException {
