ed7704410013f28b8ed57eea4640ea13faa16efc
Marcus Fihlon <marcus@fihlon.swiss>
2021-09-03 16:20:55 +0200

    :sparkles: Parse CSV file

--- src/main/java/ch/pandemieende/updater/Updater.java
+++ src/main/java/ch/pandemieende/updater/Updater.java
@@ -1,5 +1,7 @@
 package ch.pandemieende.updater;
 
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
 import edu.umd.cs.findbugs.annotations.NonNull;
 
 import java.io.File;
@@ -7,6 +9,9 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.zip.ZipInputStream;
 
 public final class Updater {
@@ -17,9 +22,14 @@ public final class Updater {
         this.zipFile = zipFile;
     }
 
-    public void doUpdate() throws IOException {
+    public void doUpdate() throws IOException, CsvValidationException {
         final var file = extractFile("COVID19VaccDosesAdministered.csv");
-        System.out.println(file);
+        try (var reader = Files.newBufferedReader(file.toPath())) {
+            final var list = parseData(reader);
+            list.stream()
+                    .map(line -> String.join(" | ", line))
+                    .forEach(System.out::println);
+        }
     }
 
     private File extractFile(@NonNull final String filename) throws IOException {
@@ -50,12 +60,24 @@ public final class Updater {
 
         var buffer = new byte[1024];
         try (var fileOutputStream = new FileOutputStream(tempFile)) {
-            for (int len = zipInputStream.read(buffer); len > 0; len = zipInputStream.read(buffer)) {
+            for (var len = zipInputStream.read(buffer); len > 0; len = zipInputStream.read(buffer)) {
                 fileOutputStream.write(buffer, 0, len);
             }
         }
 
         return tempFile;
+    }
+
+    private ArrayList<String[]> parseData(@NonNull final Reader reader) throws CsvValidationException, IOException {
+        final var list = new ArrayList<String[]>();
+        try (var csvReader = new CSVReader(reader)) {
+            for (var line = csvReader.readNext(); line != null; line = csvReader.readNext()) {
+                if ("CHFL".equals(line[1])) {
+                    list.add(line);
+                }
+            }
+        }
+        return list;
     }
 
 }
